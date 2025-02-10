b838ee6aaa8f2df005af4aba45e47a6d224b52c7
Nadman Faisal <nadman@chalmers.se>
2025-02-10 16:39:07 +0100

    Copy string to local variable

--- Exc6/main.c
+++ Exc6/main.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX 21
+
+int getLength(char *string) {
+    int counter = 0;
+    while(string[counter] != '\0' && string[counter] != '\n') {
+        counter++;
+    }
+    printf("counter: %d\n", counter);
+    return counter;
+}
+
+void stringCopy(char *destination, char *source) {
+    int stringLength = getLength(source);
+    printf("String length in method: %d\n", stringLength);
+
+    for (int i = 0; i <= stringLength; i++) {
+        if(i == stringLength) {
+            destination[i] = '\0';
+            break;
+        }
+        destination[i] = source[i];   
+    }
+}
+
 int main(int argc, char *argv[]) {
-    char string[21];
-    char destination[21];
+    char string[MAX];
+    char destination[MAX];
+    char destination2[MAX];
+
     printf("Provide as input: ");
     fgets(string, sizeof(string), stdin);
     printf("String is: %s\n", string);
 
     strcpy(destination, string);
-    printf("Destination is: %s\n", destination);
+    printf("Destination1 is: %s\n", destination);
+
+    stringCopy(destination2, string);
+    printf("Destination 2: %s\n", destination2);
 }
