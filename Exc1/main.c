21a090809759ee4f1ad4b3375ebd19004a6842fa
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2025-02-10 12:12:55 +0100

    Add commenting to Exc1

--- Exc1/main.c
+++ Exc1/main.c
@@ -6,30 +6,30 @@ For more closer to C, we could do if digitalRead(buttonPin) == HIGH or 1 and rem
 
 Questions:-
 
-int buttonPin = 7;
-int led11Pin = 11;
-int led8Pin = 8;
+int buttonPin = 7; // pin for the button
+int led11Pin = 11; // pin for the constant blinking led 
+int led8Pin = 8; // pin for the button-pressed led
 
 void setup()
 {
-  pinMode(buttonPin, INPUT);
-  pinMode(led11Pin, OUTPUT);
-  pinMode(led8Pin, OUTPUT);
+  pinMode(buttonPin, INPUT); // set pin mode for button to input
+  pinMode(led11Pin, OUTPUT); // set pin mode for constant blinking led to output
+  pinMode(led8Pin, OUTPUT);  // set pin mode for button-pressed led to output
 }
 
 void loop()
 {
 
-  byte buttonState = digitalRead(buttonPin);
-  if(buttonState == HIGH){
-    digitalWrite(led8Pin, LOW);
-  } else {
-    digitalWrite(led8Pin, HIGH);
+  byte buttonState = digitalRead(buttonPin); // read from button pin and add it to a variable called button state
+  if(buttonState == HIGH){ // if the button is on
+    digitalWrite(led8Pin, LOW); // set the button-pressed led to off
+  } else { // else
+    digitalWrite(led8Pin, HIGH); // set the button-pressed led to on
   }
   
-  digitalWrite(led11Pin, HIGH);
-  delay(1000);
-  digitalWrite(led11Pin, LOW);
-  delay(1000);
+  digitalWrite(led11Pin, HIGH); // set the constant blinking led to on
+  delay(1000); // delay for 1 second
+  digitalWrite(led11Pin, LOW); // set the constant blinking led to off
+  delay(1000); // delay for 1 second
 }
 */
