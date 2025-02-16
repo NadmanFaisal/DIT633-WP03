74fc46b72043649cdd1a32f87b43dbb424d1144c
Nadman Faisal <nadman@chalmers.se>
2025-02-16 20:03:40 +0100

    Add comments for exc 5 and 6

--- Exc5/main.c
+++ Exc5/main.c
@@ -1,43 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAX 50
+#define MAX 50              // The length of array
+#define MULTIPLIER 2        // Constant used to multiply
 
+/*
+This method prints the contents of an array by looping 
+through the array and printing its contents. 
+
+The method takes two parameters - pointer to the index 
+and the pointer to the first element of the array itself.
+*/
 void printArray(int *index, int *array) {
     printf("The numbers are: ");
-    for(*index = 0; *index < MAX; ++*index) {
-        printf("%d ", array[*index]);
+    for(*index = 0; *index < MAX; ++*index) {       // loops through the array
+        printf("%d ", array[*index]);               // and prints the integers in the array
     }
     printf("\n");
 }
 
+/*
+This method is responsible for multiplying the contents of an 
+array. 
+
+Takes two parameters - pointer to the index to loop through, and 
+pointer to the first element of the array.
+*/
 void multiplyArray(int *index, int *array) {
-    for(*index = 0; *index < MAX; ++*index) {
-        array[*index] = array[*index] * 2;
+    for(*index = 0; *index < MAX; ++*index) {               // Loops through the whole array
+        array[*index] = array[*index] * MULTIPLIER;         // and multiplies each element with MULTIPLIER
     }
 }
 
 int main(int argc, char *argv[]) {
-    int array[MAX];
-    int i;
-    int *pI = &i;
-    int randomNumber;
+    int array[MAX];                 // Initializes the array of MAX size on the stack
+    int i;                          // Variable used as index
+    int *pI = &i;                   // Pointer to the index variable
+    int randomNumber;               // Variable to hold randomly generater number
 
-    srand(time(NULL)); 
+    srand(time(NULL));              // seeds the random number generator base on time
 
-    for(*pI = 0; *pI < MAX; ++*pI) {
-        randomNumber = rand() % 99 + 1;
-        array[*pI] = randomNumber;
+    for(*pI = 0; *pI < MAX; ++*pI) {        // Loops through the array and 
+        randomNumber = rand() % 99 + 1;     // stores a random number in varialbe
+        array[*pI] = randomNumber;          // assigns the randomNumber to the array in the index
     }
 
 
-    printf("The value of the address of the array (pointer) is: %p\n", array);
-    printf("First integer in the array is (array[0]): %d\n", array[0]);
-    printf("The last integer in the array is: %d\n", array[MAX - 1]);
-    printf("The size of an integer (number of bytes) is: %ld\n", sizeof(int));
-    printf("The size of the whole array in bytes is: %ld\n", sizeof(array));
+    printf("The value of the address of the array (pointer) is: %p\n", array);      // Prints the address of the array
+    printf("First integer in the array is (array[0]): %d\n", array[0]);             // Prints the first integer in the array at position 0
+    printf("The last integer in the array is: %d\n", array[MAX - 1]);               // Prints the last integer in the array
+    printf("The size of an integer (number of bytes) is: %ld\n", sizeof(int));      // Prints the number of bytes of an integer datatype
+    printf("The size of the whole array in bytes is: %ld\n", sizeof(array));        // Prints the size of the whole array in bytes
 
-    printArray(pI, array);
-    multiplyArray(pI, array);
-    printArray(pI, array);
+    printArray(pI, array);              // Prints the array using pointer to index and pointer to the first element of an array
+    multiplyArray(pI, array);           // Multiplies the array using pointer to index and pointer to the first element of an array
+    printArray(pI, array);              // Prints the array using pointer to index and pointer to the first element of an array
 }
--- Exc6/main.c
+++ Exc6/main.c
@@ -1,47 +1,56 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 21
+#define MAX 21              // Max length of the strings
 
+/*
+This method is responsible for returning the length of a string. 
+
+Takes the pointer to the first character to the string as a parameter
+*/
 int getLength(char *string) {
-    int counter = 0;
-    while(string[counter] != '\0' && string[counter] != '\n') {
-        counter++;
+    int counter = 0;                                                    // Variable to count the length of a string
+    while(string[counter] != '\0' && string[counter] != '\n') {         // Loops until null-terminator or new-line character is not encountered
+        counter++;                                                      // Increments counter
     }
-    printf("counter: %d\n", counter);
-    return counter;
+    return counter;                                                     // Returns the counter (length of the string)
 }
 
-void stringCopy(char *destination, char *source) {
-    int stringLength = getLength(source);
-    printf("String length in method: %d\n", stringLength);
+/*
+This method copies a string form the source to the destination. 
 
-    for (int i = 0; i <= stringLength; i++) {
-        if(i == stringLength) {
-            destination[i] = '\0';
-            break;
+Takes in two parameters - pointer to the destination, and pointer 
+to the source from which the string is copied from. 
+*/
+void stringCopy(char *destination, char *source) {
+    int stringLength = getLength(source);               // Length of the source is calculated
+
+    for (int i = 0; i <= stringLength; i++) {           // Loops untill and including the end of the string
+        if(i == stringLength) {                         // If the end of the string length is reached, then 
+            destination[i] = '\0';                      // assigns a null-terminator to the destination string
+            break;                                      // breaks the loop
         }
-        destination[i] = source[i];   
+        destination[i] = source[i];                     // assigns the destination index to the source index
     }
 }
 
 int main(int argc, char *argv[]) {
-    char string[MAX];
-    char destination[MAX];
-    char destination2[MAX];
+    char string[MAX];                               // Creates a variable to hold string of MAX length on the stack
+    char destination[MAX];                          // Create the destination of MAX length on the stack
+    char destination2[MAX];                         // Creates another destination of MAX length on the stack
 
-    if(argc == 2) {    
-        *string = *argv[1];
-    } else {
-        printf("Provide as input: ");
-        fgets(string, sizeof(string), stdin);
+    if(argc == 2) {                                 // If an arguement is provided for the text file
+        *string = *argv[1];                         // Content of the text file is assigned to string
+    } else {                                        // If no arguement is provided, then 
+        printf("Provide as input: ");               // Prompts to write an input
+        fgets(string, sizeof(string), stdin);       // Stores the input to the string
     }
 
-    printf("String is: %s\n", string);
+    printf("String is: %s\n", string);              // Prints the string
 
-    strcpy(destination, string);
-    printf("Destination1 is: %s\n", destination);
+    strcpy(destination, string);                    // Copies the string to destination using 'string' library
+    printf("Destination1 is: %s\n", destination);   // Prints the destination to ensure expected outcome
 
-    stringCopy(destination2, string);
-    printf("Destination 2: %s\n", destination2);
+    stringCopy(destination2, string);               // Copies the string to destination2 using new method
+    printf("Destination 2: %s\n", destination2);    // Prints the destination2 to ensure expected outcome
 }
