1df28d9e96d61ac0aceca300d1991999f3d7ecea
Nadman Faisal <nadman@chalmers.se>
2025-02-16 19:18:25 +0100

    Add comments for submission

--- Exc1/main/main.ino
+++ Exc1/main/main.ino
@@ -1,19 +1,19 @@
-int ledPin = 8;
-int ledPin2 = 7;
-int buttonPin = 2;
+int ledPin = 8;					  // Pin for the blinking LED
+int ledPin2 = 7;				  // Pin for the switch LED
+int buttonPin = 2;				// Pin for button
 void setup() {
-  	pinMode(buttonPin, INPUT);
-  	pinMode(ledPin2, OUTPUT);
-    pinMode(ledPin,OUTPUT);
+  	pinMode(buttonPin, INPUT);        // Initializes button pin as INPUT
+  	pinMode(ledPin2, OUTPUT);	        // Initizlises OUTPUT for shwoing light
+    pinMode(ledPin,OUTPUT);		        // Initizlises OUTPUT for shwoing light
 }
 void loop()	{  
-  if(digitalRead(buttonPin) == LOW) {
-  	digitalWrite(ledPin2, LOW);
-  } else {
-  	digitalWrite(ledPin2, HIGH);
+  if(digitalRead(buttonPin) == HIGH) {			// If button is pressed
+  	digitalWrite(ledPin2, HIGH);				    // light is off
+  } else {										              // else if button not pressed
+  	digitalWrite(ledPin2, LOW);					    // light is on
   }
-    digitalWrite(ledPin,HIGH);
-    delay(1000);
-    digitalWrite(ledPin,LOW);
-    delay(1000);
+    digitalWrite(ledPin,HIGH);				// turns light on
+    delay(1000);							        // waits one second
+    digitalWrite(ledPin,LOW);				  // turns light off
+    delay(1000);							        // waits one second
 }
--- Exc2/main/main.ino
+++ Exc2/main/main.ino
@@ -1,101 +1,105 @@
-// C++ code
-//
+int redLED = 4;           // pin for red LED
+int greenLED = 8;         // pin for green LED
+int yellowLED = 7;        // pin for yellow LED
 
-int redLED = 4;
-int greenLED = 8;
-int yellowLED = 7;
+int tempPin = A0;         // analog pin for temperature sensor
+int lightPin = A1;        // analog pin for photoresistor (light sensor)
 
-int tempPin = A0;
-int lightPin = A1;
+int period = 1;           // period delay in seconds
+
 void setup()
 {
-  pinMode(redLED, OUTPUT);
-  pinMode(greenLED, OUTPUT);
-  pinMode(yellowLED, OUTPUT);
-  Serial.begin(9600);
+  pinMode(redLED, OUTPUT);        // Initializes as output to display light
+  pinMode(greenLED, OUTPUT);      // Initializes as output to display light
+  pinMode(yellowLED, OUTPUT);     // Initializes as output to display light
+  Serial.begin(9600);             // Initializes the serial to print values
 }
 
 void loop()
 {
-  float tempReading = analogRead(tempPin);
-  float voltage = tempReading * (5000 / 1024.0);
-  float temperature = (voltage - 500) / 10;
+  float tempReading = analogRead(tempPin);                // stores the readings from analog tempPin
+  float voltage = tempReading * (5000 / 1024.0);          // converts the analog reading to voltage
+  float temperature = (voltage - 500) / 10;               // converts the voltage to temperature
   
-  float lightReading = analogRead(lightPin);
-  float intensity = map(lightReading, 6, 679, 0, 100);
+  float lightReading = analogRead(lightPin);              // Reads from analog pin of lightPin
+  float intensity = map(lightReading, 6, 679, 0, 100);    // Maps the min and max of light reading to percentage, indicating intensity
   
+  // Prints the temperature value
   Serial.print("Temperature: ");
   Serial.print(temperature);
   Serial.print("\n");
   
+  // Prints the intensity value
   Serial.print("Intensity: ");
   Serial.print(intensity);
   Serial.print("\n");
   
 // =========================================
   
-  if(intensity < 1) {
-    if(temperature < -12) {
-      digitalWrite(greenLED, HIGH);
-      digitalWrite(redLED, LOW);
-      digitalWrite(yellowLED, LOW);
-    } else {
-      digitalWrite(greenLED, LOW);
-      digitalWrite(redLED, HIGH);
-      digitalWrite(yellowLED, LOW);
+  /* Lights LEDs depending on the dependency between intensity and temperature */
+  
+  if(intensity < 1) {                                 // If intensity is less than 0
+    if(temperature < -12) {                           // If temperature is less than -12 degrees
+      digitalWrite(greenLED, HIGH);                   // green LED turned on
+      digitalWrite(redLED, LOW);                      // red LED turned off
+      digitalWrite(yellowLED, LOW);                   // yellow LED turned off
+    } else {                                          // else if temperature temperature is greater than or equal to -12 degrees
+      digitalWrite(greenLED, LOW);                    // turns off green LED
+      digitalWrite(redLED, HIGH);                     // turns on red LED
+      digitalWrite(yellowLED, LOW);                   // turns off yellow LED
     }
   }
 
 // =========================================
   
-  else if(intensity >= 1 && intensity <= 20) {
-    if(temperature < -12) {
-      	digitalWrite(greenLED, LOW);
-      	digitalWrite(redLED, LOW);
-      	digitalWrite(yellowLED, HIGH);
-    } else if(temperature > 0) {
-      	digitalWrite(greenLED, LOW);
-      	digitalWrite(redLED, HIGH);
-      	digitalWrite(yellowLED, LOW);
-    } else {
-    	digitalWrite(greenLED, HIGH);
-      	digitalWrite(redLED, LOW);
-      	digitalWrite(yellowLED, LOW);
+  else if(intensity >= 1 && intensity <= 20) {      // If intensity is within 1 to 20 range
+    if(temperature < -12) {                         // and if temp is less than -12
+      	digitalWrite(greenLED, LOW);                // turns off green LED
+      	digitalWrite(redLED, LOW);                  // turns off red LED
+      	digitalWrite(yellowLED, HIGH);              // turns on yellow LED
+    } else if(temperature > 0) {                    // if temperature is greater than 0
+      	digitalWrite(greenLED, LOW);                // turns off green LED
+      	digitalWrite(redLED, HIGH);                 // turns on red LED
+      	digitalWrite(yellowLED, LOW);               // turns off yellow LED
+    } else {                                        // else,
+    	digitalWrite(greenLED, HIGH);                 // turns on green LED
+      	digitalWrite(redLED, LOW);                  // turns off red LED
+      	digitalWrite(yellowLED, LOW);               // turns off yellow LED
     }
   }
   
 // =========================================
-  else if(intensity >= 21 && intensity <= 60) {
-    if(temperature < 0) {
-    	digitalWrite(greenLED, LOW);
-      	digitalWrite(redLED, LOW);
-      	digitalWrite(yellowLED, HIGH);
-    } else if(temperature > 20) {
-    	digitalWrite(greenLED, LOW);
-      	digitalWrite(redLED, HIGH);
-      	digitalWrite(yellowLED, LOW);
+  else if(intensity >= 21 && intensity <= 60) {     // If intensity is within 21 to 60 range
+    if(temperature < 0) {                           // and if temp is less than 0
+    	  digitalWrite(greenLED, LOW);                // turns off green LED
+      	digitalWrite(redLED, LOW);                  // turns off red LED
+      	digitalWrite(yellowLED, HIGH);              // turns on yellow LED
+    } else if(temperature > 20) {                   // if temperature is greater than 20
+    	  digitalWrite(greenLED, LOW);                // turns off green LED
+      	digitalWrite(redLED, HIGH);                 // turns on red LED
+      	digitalWrite(yellowLED, LOW);               // turns off yellow LED
     } else {
-    	digitalWrite(greenLED, HIGH);
-      	digitalWrite(redLED, LOW);
-      	digitalWrite(yellowLED, LOW);
+    	  digitalWrite(greenLED, HIGH);               // turns on green LED
+      	digitalWrite(redLED, LOW);                  // turns off red LED
+      	digitalWrite(yellowLED, LOW);               // turns off yellow LED
     }
   }
   
 // =========================================
   
-  else {
-    if(temperature >= 21) {
-    	digitalWrite(greenLED, HIGH);
-      	digitalWrite(redLED, LOW);
-      	digitalWrite(yellowLED, LOW);
-    } else {
-    	digitalWrite(greenLED, LOW);
-      	digitalWrite(redLED, LOW);
-      	digitalWrite(yellowLED, HIGH);
+  else {                                            // else if intensity is greater than 60
+    if(temperature >= 21) {                         // and if temperature is greater than 20
+    	digitalWrite(greenLED, HIGH);                 // turns on green LED
+      	digitalWrite(redLED, LOW);                  // turns off red LED
+      	digitalWrite(yellowLED, LOW);               // turns off yellow LED
+    } else {                                        // if temp is less than 21
+    	digitalWrite(greenLED, LOW);                  // turns off green LED
+      	digitalWrite(redLED, LOW);                  // turns off red LED
+      	digitalWrite(yellowLED, HIGH);              // turns on yellow LED
     }
   }
   
 
   
-  delay(1000);
+  delay(period * 1000);               // Delays the program by the seconds mentioned in period variable
 }
