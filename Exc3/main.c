e8a9be3344a463cfeb66fac1035a6f03b4f94345
Hasan Zahid <guszahidha@student.gu.se>
2025-02-17 15:42:38 +0100

    added comments for exc3

--- Exc3/main.c
+++ Exc3/main.c
@@ -1,23 +1,23 @@
-#define TEMP_PIN A0
+#define TEMP_PIN A0 // defining temperature pin
 
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(TEMP_PIN, INPUT);
+  Serial.begin(9600); // initialize serial
+  pinMode(TEMP_PIN, INPUT); // initialize temp pin
 }
 
 void loop()
 {
-  float temp = analogRead(TEMP_PIN);
+  float temp = analogRead(TEMP_PIN); // store the analog value in temp
   
-  float volt = (temp) * 4.9;
-  temp = (volt - 500)/10;
+  float volt = (temp) * 4.9; // convert the analog value to volt
+  temp = (volt - 500)/10; // convert the volt to degree (C)
   
   
-  Serial.println("Voltage: ");
-  Serial.println(volt);
-  Serial.println("Temp C: ");
-  Serial.println(temp);
-  delay(500);
+  Serial.println("Voltage: "); // print line for voltage
+  Serial.println(volt); // print volt value
+  Serial.println("Temp C: "); // print line for temp c
+  Serial.println(temp); // print the temp value
+  delay(500); // delay of 0.5 seconds
   
 }
